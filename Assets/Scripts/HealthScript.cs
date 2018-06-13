756516a68fca261a7e643fcff1c91f83dee95e63
gc42 <gc3d.42@gmail.com>
2018-06-13 11:18:54 +0200

    * Assets/Scenes/2DGame00.unity: 	* Assets/Scripts/HealthScript.cs: First version of the script

---
+++ Assets/Scripts/HealthScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Handle hitpoints and damages
+/// </summary>
+
+public class HealthScript : MonoBehaviour
+{
+	/// <summary>
+	/// Total hitpoints
+	/// </summary>
+	public int hp = 1;
+
+	/// <summary>
+	/// Enemy or player?
+	/// </summary>
+	public bool isEnemy = true;
+
+	/// <summary>
+	/// Inflict damage and check if the object should be destroyed
+	/// </summary>
+	/// <param name="damageCount"></param>
+	public void Damage(int damageCount)
+	{
+		hp -= damageCount;
+
+		if (hp <= 0)
+		{
+			// Dead
+			Destroy(gameObject);
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D otherCollider)
+	{
+		// Is this collision a shot?
+		ShotScript shot = otherCollider.gameObject.GetComponent<ShotScript>();
+		if (shot != null)
+		{
+			// Avoid friendly fire
+			if (shot.isEnemyShot != isEnemy)
+			{
+				Damage(shot.damage);
+
+				// Destroy the shot
+				Destroy(shot.gameObject);
+			}
+		}
+	}
+}
