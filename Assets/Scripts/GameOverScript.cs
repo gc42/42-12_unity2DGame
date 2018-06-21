c05efb37c3aa3c1c3d6e7c93e4e0aa4c333602cb
gc42 <gc3d.42@gmail.com>
2018-06-21 16:03:00 +0200

    Mise a jour du poulpy 2DGame

---
+++ Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Restart or quit the game
+/// </summary>
+public class GameOverScript : MonoBehaviour
+{
+	private GameObject[] buttons;
+
+	private void Awake()
+	{
+		// Get the buttons
+		buttons = GameObject.FindGameObjectsWithTag("button");
+
+		// Disable them
+		HideButtons();
+		Time.timeScale = 1.0f;
+	}
+
+
+	public void HideButtons()
+	{
+		foreach (var b in buttons)
+		{
+			b.SetActive(false);
+		}
+	}
+
+	public void ShowButtons()
+	{
+		foreach (var b in buttons)
+		{
+			b.SetActive(true);
+		}
+		Time.timeScale = 0.1f;
+	}
+
+	public void ExitToMenu()
+	{
+		// Load the menu
+		SceneManager.LoadScene(0);
+	}
+
+	public void RestartGame()
+	{
+		// Restart the level
+		SceneManager.LoadScene(1);
+	}
+
+
+
+}
